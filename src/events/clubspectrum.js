847f643d64938004c39da98a868f0dd27d696f2d
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-07-17 02:00:22 -0400

    first commit

---
+++ src/events/clubspectrum.js
@@ -0,0 +1,23 @@
+const { PermissionFlagsBits } = require("discord.js");
+const fs = require("fs/promises");
+
+module.exports = {
+  name: "messageCreate",
+  async execute(message) {
+
+    if (message.author.bot || !message.guild) return;
+    if (!message.guild.members.me.permissionsIn(message.channel.id).has(PermissionFlagsBits.SendMessages)) return;
+        
+    const voicelinesJsonData = await fs.readFile("data/voicelines.json", {encoding: "utf8"});
+    const voicelinesMap = JSON.parse(voicelinesJsonData);
+    const voicelines = voicelinesMap["clubspectrum"];
+    const voiceline = voicelines[Math.floor(Math.random() * voicelines.length)];
+
+    const chance = Math.random() * 650;
+    if (chance < 648) {
+      return;
+    } else {
+      message.channel.send(`${voiceline}`);
+    };
+  },
+};
---
+++ src/events/gamblingroom.js
@@ -0,0 +1,23 @@
+const { PermissionFlagsBits } = require("discord.js");
+const fs = require("fs/promises");
+
+module.exports = {
+  name: "messageCreate",
+  async execute(message) {
+
+    if (message.author.bot || !message.guild) return;
+    if (!message.guild.members.me.permissionsIn(message.channel.id).has(PermissionFlagsBits.SendMessages)) return;
+        
+    const voicelinesJsonData = await fs.readFile("data/voicelines.json", {encoding: "utf8"});
+    const voicelinesMap = JSON.parse(voicelinesJsonData);
+    const voicelines = voicelinesMap["gamblingroom"];
+    const voiceline = voicelines[Math.floor(Math.random() * voicelines.length)];
+
+    const chance = Math.random() * 950;
+    if (chance < 948) {
+      return;
+    } else {
+      message.channel.send(`${voiceline}`);
+    };
+  },
+};
